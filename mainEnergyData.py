fe75be1cb143c0bdf4f7de04895d6b98a9c47c85
Dimitar Dimov <dimitar.dimov9204@gmail.com>
2020-05-12 18:03:04 +0100

    Update mainEnergyData.py

--- mainEnergyData.py
+++ mainEnergyData.py
@@ -18,6 +18,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
+
 import numpy as np
 
 edata = pd.read_csv("energydata_complete.csv", index_col = ["date"], parse_dates = ["date"])
@@ -77,3 +78,56 @@ def plot_exponential_smoothing(series, alphas):
     plt.grid(True);
 
 plot_exponential_smoothing(edata.T2, [0.05, 0.3])
+
+def double_exponential_smoothing(series, alpha, beta):
+
+    result = [series[0]]
+    for n in range(1, len(series)+1):
+        if n == 1:
+            level, trend = series[0], series[1] - series[0]
+        if n >= len(series): # forecasting
+            value = result[-1]
+        else:
+            value = series[n]
+        last_level, level = level, alpha * value + (1 - alpha) * (level + trend)
+        trend = beta * (level - last_level) + (1 - beta) * trend
+        result.append(level + trend)
+    return result
+
+def plot_double_exponential_smoothing(series, alphas, betas):
+     
+    plt.figure(figsize=(17, 8))
+    for alpha in alphas:
+        for beta in betas:
+            plt.plot(double_exponential_smoothing(series, alpha, beta), label="Alpha {}, beta {}".format(alpha, beta))
+    plt.plot(series.values, label = "Actual")
+    plt.legend(loc="best")
+    plt.axis('tight')
+    plt.title("Double Exponential Smoothing")
+    plt.grid(True)
+    
+def tsplot(y, lags=None, figsize=(12, 7), syle='bmh'):
+    
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+        
+    with plt.style.context(style='bmh'):
+        fig = plt.figure(figsize=figsize)
+        layout = (2,2)
+        ts_ax = plt.subplot2grid(layout, (0,0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1,0))
+        pacf_ax = plt.subplot2grid(layout, (1,1))
+        
+        y.plot(ax=ts_ax)
+        p_value = sm.tsa.stattools.adfuller(y)[1]
+        ts_ax.set_title('Time Series Analysis Plots\n Dickey-Fuller: p={0:.5f}'.format(p_value))
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax)
+        plt.tight_layout()
+        
+tsplot(edata.T2, lags=30)
+
+# Take the first difference to remove to make the process stationary
+data_diff = edata.T2 - edata.T2.shift(1)
+
+tsplot(data_diff[1:], lags=30)
